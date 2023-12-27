5be4c3dbe25d27e0f0fe86a0766ba44d3d2dba02
JasmithaDamarla <138194487+JasmithaDamarla@users.noreply.github.com>
2023-12-27 22:26:53 +0530

    updated by adding an exception
    Added AccessDeniedException

--- src/main/java/com/authorize/exceptions/RestControllerExceptionHandler.java
+++ src/main/java/com/authorize/exceptions/RestControllerExceptionHandler.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,54 +15,61 @@ import org.springframework.web.context.request.WebRequest;
 
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 @RestControllerAdvice
 public class RestControllerExceptionHandler {
-	
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(value=HttpStatus.BAD_REQUEST)
-	public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException func, WebRequest req) {
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException func,
+			WebRequest req) {
 		List<String> inpErrors = new ArrayList<>();
-		func.getAllErrors()
-			.forEach(error -> 
-				inpErrors.add(error.getDefaultMessage()));
+		func.getAllErrors().forEach(error -> inpErrors.add(error.getDefaultMessage()));
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date().toString(),
 				HttpStatus.BAD_REQUEST.name(), inpErrors.toString(), req.getDescription(false));
-		log.info("Method arugument not valid exception raised : {}",inpErrors.toString());
-		return new ResponseEntity<>(exceptionResponse,HttpStatus.BAD_REQUEST);
+		log.info("Method arugument not valid exception raised : {}", inpErrors.toString());
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
-	
+
 	@ExceptionHandler(UnAuthorizedException.class)
 	public ResponseEntity<ExceptionResponse> handleUserException(UnAuthorizedException userException, WebRequest req) {
-		log.info("Unauthorised exception has been raised : {}",userException.getMessage());
+		log.info("Unauthorised exception has been raised : {}", userException.getMessage());
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date().toString(),
-				HttpStatus.BAD_REQUEST.name(),userException.getMessage(), req.getDescription(false));
-		return new ResponseEntity<>(exceptionResponse,HttpStatus.UNAUTHORIZED);
+				HttpStatus.BAD_REQUEST.name(), userException.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.UNAUTHORIZED);
 	}
-	
+
 	@ExceptionHandler(UserNotFoundException.class)
 	public ResponseEntity<ExceptionResponse> handleUserException(UserNotFoundException userException, WebRequest req) {
-		log.info("UserNotFoundException exception has been raised : {}",userException.getMessage());
+		log.info("UserNotFoundException exception has been raised : {}", userException.getMessage());
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date().toString(),
-				HttpStatus.BAD_REQUEST.name(),userException.getMessage(), req.getDescription(false));
-		return new ResponseEntity<>(exceptionResponse,HttpStatus.BAD_REQUEST);
+				HttpStatus.BAD_REQUEST.name(), userException.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
-	
+
 	@ExceptionHandler(SignUpFailedException.class)
 	public ResponseEntity<ExceptionResponse> handleTraineeException(SignUpFailedException signupfail, WebRequest req) {
-		log.info("signupfailed exception has been raised : {}",signupfail.getMessage());
+		log.info("signupfailed exception has been raised : {}", signupfail.getMessage());
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date().toString(),
-				HttpStatus.BAD_REQUEST.name(),signupfail.getMessage(), req.getDescription(false));
-		return new ResponseEntity<>(exceptionResponse,HttpStatus.BAD_REQUEST);
+				HttpStatus.BAD_REQUEST.name(), signupfail.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
-	
+
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<ExceptionResponse> handleAccessDeniedException(AccessDeniedException accessDeniedException,
+			WebRequest req) {
+		log.info("Access denied exception has been raised: {}", accessDeniedException.getMessage());
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date().toString(), HttpStatus.FORBIDDEN.name(),
+				accessDeniedException.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
+	}
+
 	@ExceptionHandler(RuntimeException.class)
 	public ResponseEntity<ExceptionResponse> handleRuntimeException(RuntimeException runtime, WebRequest req) {
-		log.info("Run time exception has been raised : {}",runtime.getMessage());
+		log.info("Run time exception has been raised : {}", runtime.getMessage());
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date().toString(),
-				HttpStatus.BAD_REQUEST.name(),runtime.getMessage(), req.getDescription(false));
-		return new ResponseEntity<>(exceptionResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+				HttpStatus.BAD_REQUEST.name(), runtime.getMessage(), req.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
